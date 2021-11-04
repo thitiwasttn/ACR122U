a4b1868b23356516159ac28c5bba18620553f551
GA007007\Admin <thitiwas.n@g-able.com>
2021-11-04 21:22:07 +0700

    up

---
+++ src/main/java/com/thitiwas/acr122u/ACR122U.java
@@ -0,0 +1,9 @@
+package com.thitiwas.acr122u;
+
+public class ACR122U {
+    public static void main(String[] args) {
+        System.out.println("============= System Start =============");
+
+        System.out.println("============= End Start =============");
+    }
+}
