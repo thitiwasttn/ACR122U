e9f4d49a54c96ad811ddd97abc744fe2e42bc801
Thitiwas <55071973+thitiwasttn@users.noreply.github.com>
2021-11-05 17:39:55 +0700

    up

--- src/main/java/com/thitiwas/acr122u/frame/Gui.java
+++ src/main/java/com/thitiwas/acr122u/frame/Gui.java
@@ -139,4 +139,10 @@ public class Gui extends JFrame {
         builder.start();
         System.exit(0);
     }
+
+    public void setError() {
+        resetButton.setText("\u0e40\u0e01\u0e34\u0e14\u0e02\u0e49\u0e2d\u0e1c\u0e34\u0e14\u0e1e\u0e25\u0e32\u0e14 \u0e01\u0e14\u0e17\u0e35\u0e48\u0e19\u0e35\u0e48\u0e40\u0e1e\u0e37\u0e48\u0e2d\u0e41\u0e01\u0e49\u0e44\u0e02");
+        resetButton.setForeground(Color.RED);
+        resetButton.setEnabled(true);
+    }
 }
